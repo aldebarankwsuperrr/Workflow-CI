457b677be6e0a79f0e2af89736dd4bf9ebeaf274
fahrulfrmn_ <seputargame3576@gmail.com>
2025-06-30 20:56:04 +0700

    fix the modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -2,37 +2,44 @@ import mlflow
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import random
 import numpy as np
+import os
+import warnings
+import sys
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-mlflow.set_experiment("Personality Classification")
-
-data = pd.read_csv("personality_dataset_preprocessing.csv")
-
-X_train, X_test, y_train, y_test = train_test_split(
-    data.drop("Personality", axis=1),
-    data["Personality"],
-    random_state=42,
-    test_size=0.2
-)
-
-input_example = X_train[0:5]
-
-print("mlflow start")
-with mlflow.start_run():
-    # log parameters
-    n_estimators= 505
-    max_depth = 37
-    mlflow.autolog()
-    # Train model
-    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-    )
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
     
-    model.fit(X_train, y_train)
-    # Log metrics
-    accuracy = model.score(X_test, y_test)
-    mlflow.log_metric("accuracy", accuracy)
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "personality_dataset_preprocessing.csv")
+    data = pd.read_csv(file_path)
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        data.drop("Personality", axis=1),
+        data["Personality"],
+        random_state=42,
+        test_size=0.2
+    )
+    input_example = X_train[0:5]
+    n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 505
+    max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 37
+    
+    with mlflow.start_run():
+        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+        model.fit(X_train, y_train)
+        
+        predicted_qualities = model.predict(X_test)
+        
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=input_example
+        )
+        # train model
+        model.fit(X_train, y_train)
+        
+        # Log Metrics
+        accuracy = model.score(X_test, y_test)
+        mlflow.log_metric("accuracy", accuracy)
+        
